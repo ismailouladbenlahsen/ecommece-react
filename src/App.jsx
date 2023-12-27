b92ee6f55fb4a6f35992ad12ff2c5ea0d4993c32
Ismail Oulad Ben Lahsen <ismailouladbenlahsen@gmail.com>
2023-12-27 13:28:45 +0100

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/product/Home/Home";
+import Categories from "./pages/Categories/Categories";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+const Layout = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/products/:id",
+        element: <Categories />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
